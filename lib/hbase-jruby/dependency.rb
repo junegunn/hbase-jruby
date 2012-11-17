efa876b34d650bfdeb022a9d0788faaa45e11c5a
Junegunn Choi <junegunn.c@gmail.com>
2012-11-18 00:08:07 +0900

    download pom.xml from github head

--- lib/hbase-jruby/dependency.rb
+++ lib/hbase-jruby/dependency.rb
@@ -1,4 +1,5 @@
 require 'java'
+require 'open-uri'
 require 'tempfile'
 
 # HBase connection
@@ -10,6 +11,7 @@ class HBase
     # @return [Array<String>] Loaded JAR files
     def resolve_dependency! dist, verbose = false
       silencer = verbose ? '' : '> /dev/null'
+      tfs = []
       jars =
         if dist == :hbase
           # Check for hbase executable
@@ -21,18 +23,36 @@ class HBase
           mvn = `which mvn`
           raise RuntimeError, "Cannot find executable `mvn`" if mvn.empty?
 
+          distname = dist.downcase.sub(/\.xml$/, '')
           path = [
+            File.expand_path("../pom/#{distname}.xml", __FILE__),
             dist.to_s,
-            File.expand_path("../pom/#{dist.downcase}.xml", __FILE__)
           ].select { |f| File.exists? f }.first
 
-          # TODO: remote fetch from github head
+          # Try github head
+          unless path
+            begin
+              xml = open("https://raw.github.com/junegunn/hbase-jruby/master/lib/hbase-jruby/pom/#{distname}.xml").read
+              tfs << tf = Tempfile.new("#{distname}.xml")
+              tf.close(false)
+              path = tf.path
+              File.open(path, 'w') do |f|
+                f << xml
+              end
+            rescue OpenURI::HTTPError => e
+              # No such distribution anywhere
+            end
+          end
+
           raise ArgumentError, "Invalid distribution: #{dist}" unless path
 
           # Download dependent JAR files and build classpath string
-          tf = Tempfile.new 'hbase-jruby-classpath'
-          tf.close(false) # Unlink later
+          tfs << tf = Tempfile.new('hbase-jruby-classpath')
+          tf.close(false)
           system "mvn org.apache.maven.plugins:maven-dependency-plugin:2.5.1:resolve org.apache.maven.plugins:maven-dependency-plugin:2.5.1:build-classpath -Dsilent=true -Dmdep.outputFile=#{tf.path} -f #{path} #{silencer}"
+
+          raise RuntimeError.new("Error occurred. Set verbose parameter to see the log.") unless $?.exitstatus == 0
+
           File.read(tf.path).split(':')
         end
 
@@ -41,7 +61,7 @@ class HBase
         require jar
       end
     ensure
-      tf.unlink if tf
+      tfs.each { |tf| tf.unlink rescue nil }
     end
   end
 end
