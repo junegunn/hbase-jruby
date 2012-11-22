c43bcdc482064fdfef744b6bc4ebbc4a10f30a1b
Junegunn Choi <junegunn.c@gmail.com>
2012-11-22 15:37:18 +0900

    thread-safety

--- lib/hbase-jruby/admin.rb
+++ lib/hbase-jruby/admin.rb
@@ -1,12 +1,16 @@
+require 'thread'
+
 class HBase
 module Admin
 private
   def with_admin
-    begin
-      admin = HBaseAdmin.new(@config)
-      yield admin
-    ensure
-      admin.close if admin
+    (@admin_mutex ||= Mutex.new).synchronize do
+      begin
+        admin = HBaseAdmin.new(@config)
+        yield admin
+      ensure
+        admin.close if admin
+      end
     end
   end
 end#Admin
