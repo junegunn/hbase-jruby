08e345df4bbf859bba67f134ccfb9e36c50a614b
Junegunn Choi <junegunn.c@gmail.com>
2012-12-18 13:41:59 +0900

    removes mutex

--- lib/hbase-jruby/admin.rb
+++ lib/hbase-jruby/admin.rb
@@ -1,25 +1,27 @@
-require 'thread'
-
 class HBase
 # @private
 module Admin
 private
   def with_admin
-    (@admin_mutex ||= Mutex.new).synchronize do
-      begin
-        admin = HBaseAdmin.new(@config)
-        yield admin
-      ensure
-        admin.close if admin
-      end
+    begin
+      admin = HBaseAdmin.new(@config)
+      yield admin
+    ensure
+      admin.close if admin
     end
   end
 
-  def wait_async_admin admin
+  def wait_async_admin admin, &block
+    prev_yet = nil
     while true
       pair  = admin.getAlterStatus(@name.to_java_bytes)
       yet   = pair.getFirst
       total = pair.getSecond
+
+      if block && yet != prev_yet
+        block.call (total - yet), total
+        prev_yet = yet
+      end
 
       break if yet == 0
       sleep 1
